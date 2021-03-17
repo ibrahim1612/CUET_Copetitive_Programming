cc8b63e918816f6bef3d3e4f347dee157d445997
ibrahim1612 <78096031+ibrahim1612@users.noreply.github.com>
2021-03-17 23:26:16 +0600

    Update LO - Circle in Square.c

--- LO - Circle in Square.c
+++ LO - Circle in Square.c
@@ -2,15 +2,15 @@
 #include <math.h>
 main()
 {
-    float T,circle_area,blue_part,square_area,pi=2*acos(0),r;
-    scanf("%f",&T);
+    double T,circle_area,blue_part,square_area,pi=2*acos(0),r;
+    scanf("%lf",&T);
     for(int i=1;i<=T;i++)
     {
-        scanf("%f",&r);
+        scanf("%lf",&r);
         circle_area=pi*r*r;
         square_area=(r*2)*(r*2);
         blue_part=square_area-circle_area;
-        printf("Case %d: %.2f\n",i,blue_part);
+        printf("Case %d: %.2lf\n",i,blue_part);
     }
     return 0;
 }
